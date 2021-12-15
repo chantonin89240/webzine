3a35023e2fea67f0f7b6e7e9870d29db06cf077f
Kriqx <jeanchristophe.pouzin@diiage.org>
2021-12-15 12:10:58 +0100

    Ajout du projet test et du projet entity + bootstrap et rename projet

--- Webzine.Entity/Artiste.cs
+++ Webzine.Entity/Artiste.cs
@@ -2,6 +2,13 @@
 {
     public class Artiste
     {
+        public int  IdArtiste { get; set; }
+        public string Nom { get; set; }
+
+        public string Biographie { get; set; }
+        public List<Titre> Titres  { get; set; }
+
+
 
     }
 }
