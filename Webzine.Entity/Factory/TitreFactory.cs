78d7f08b6795780bb652239f5e6e741c490cb23f
Vincent Simonin <Vincent.Simonin@diiage.org>
2021-12-16 16:57:44 +0100

    Push pour la soirée

--- Webzine.Entity/Factory/TitreFactory.cs
+++ Webzine.Entity/Factory/TitreFactory.cs
@@ -12,7 +12,59 @@ namespace Webzine.Entity.Factory
         {
             return new List<Titre>()
             {
-                new Titre{IdTitre = 1, IdArtiste = 1, Artiste = new Artiste(), Libelle = "queen", Chronique = "blabla", UrlJaquette = "", UrlEcoute = "", Lien = "",DateCreation = new DateTime(2021-12-10), DateSortie =  new DateTime(2021-12-11), Duree = 180, NbLectures = 20, NbLikes = 7, Commentaires = new List<Commentaire>(), TitresStyles = new List<TitreStyle>()}
+                new Titre(){
+                    IdTitre = 1, 
+                    IdArtiste = 1, 
+                    Artiste = new Artiste(), 
+                    Libelle = "747 - Aurora Centralis", 
+                    Chronique = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", 
+                    UrlJaquette = "https://images-eu.ssl-images-amazon.com/images/I/61dsMoSVAXL._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_SCLZZZZZZZ_FMpng_BG255%2C255%2C255.jpg", 
+                    UrlEcoute = "", 
+                    Lien = "",
+                    DateCreation = new DateTime(2021-12-10), 
+                    DateSortie =  new DateTime(2021-12-11), 
+                    Duree = 180, 
+                    NbLectures = 20, 
+                    NbLikes = 7, 
+                    Commentaires = new List<Commentaire>(), 
+                    TitresStyles = new List<TitreStyle>()
+                },      
+                new Titre()
+                {
+                    IdTitre = 2,
+                    IdArtiste = 2,
+                    Artiste = new Artiste(),
+                    Libelle = "Röyksopp - Sordid Affai - Maceo Plex Remix",
+                    Chronique = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    UrlJaquette = "https://img.discogs.com/Lt0SDzF84Mi_dDAnT_kb2fN7I-A=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6190573-1413305948-2894.jpeg.jpg",
+                    UrlEcoute = "",
+                    Lien = "",
+                    DateCreation = new DateTime(2021 - 12 - 10),
+                    DateSortie = new DateTime(2021 - 12 - 11),
+                    Duree = 180,
+                    NbLectures = 20,
+                    NbLikes = 7,
+                    Commentaires = new List<Commentaire>(),
+                    TitresStyles = new List<TitreStyle>()
+                },
+                new Titre()
+                {
+                    IdTitre = 3,
+                    IdArtiste = 3,
+                    Artiste = new Artiste(),
+                    Libelle = "Jean-Michel Jarre - Oxygene, Pt1",
+                    Chronique = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+                    UrlJaquette = "https://www.guettapen.com/wp-content/uploads/2016/12/Oxygene-3.jpg",
+                    UrlEcoute = "",
+                    Lien = "",
+                    DateCreation = new DateTime(2021 - 12 - 10),
+                    DateSortie = new DateTime(2021 - 12 - 11),
+                    Duree = 180,
+                    NbLectures = 20,
+                    NbLikes = 7,
+                    Commentaires = new List<Commentaire>(),
+                    TitresStyles = new List<TitreStyle>()
+                }
             };
         }
     }
