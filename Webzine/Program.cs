7c9133c624db38945eafe72eafbbd2a0017bb563
Vincent Simonin <Vincent.Simonin@diiage.org>
2021-12-16 16:57:44 +0100

    Maj pour présentation page d'acceuil Reste défaut sur position ou resizing image card et limitation du texte

--- Webzine/Program.cs
+++ Webzine/Program.cs
@@ -3,12 +3,18 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
+app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
-app.UseStaticFiles();
 
-//app.MapGet("/", () => "Hello World!");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 
 app.Run();
