e055fed838dc7844c512e0044f6b10bf409eccf6
Vincent Simonin <Vincent.Simonin@diiage.org>
2021-12-17 09:00:37 +0100

    modif routage

--- Webzine/Program.cs
+++ Webzine/Program.cs
@@ -6,12 +6,13 @@ var app = builder.Build();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
 
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
       name: "areas",
       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapDefaultControllerRoute();
 });
 app.Run();
