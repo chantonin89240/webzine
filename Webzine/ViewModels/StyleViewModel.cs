ec46fbfb8f8c33fab48ab040e63e3aea417a475a
Vincent Simonin <Vincent.Simonin@diiage.org>
2022-01-05 16:11:12 +0100

    Maj TitreController

---
+++ Webzine/ViewModels/StyleViewModel.cs
@@ -0,0 +1,12 @@
+﻿ using Webzine.Entity;
+using Webzine.Entity.Factory;
+
+namespace Webzine.WebApplication.ViewModels
+{
+    public class StyleViewModel
+    {
+        public List<Style> Styles { get; set; }
+
+        public Style Style { get; set; }
+    }
+}
