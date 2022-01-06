f5100186a2a52743798aa950698716b1dad790a2
Vincent Simonin <Vincent.Simonin@diiage.org>
2022-01-06 16:56:25 +0100

    WebzineDbContext sqlite ok Reste un défaut sur la clé composé de TitreStyle

--- Webzine/Program.cs
+++ Webzine/Program.cs
@@ -8,8 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-//builder.Services.AddScoped<IArtisteRepository, LocalArtisteRepository>();
+builder.Services.AddScoped<IArtisteRepository, LocalArtisteRepository>();
 builder.Services.AddScoped<ITitreRepository, LocalTitreRepository>();
+builder.Services.AddScoped<IStyleRepository, LocalStyleRepository>();
+builder.Services.AddScoped<ICommentaireRepository, LocalCommentaireRepository>();
 
 NLog.LogManager.LoadConfiguration("nlog.config");
 
@@ -17,10 +19,12 @@ builder.Services.AddDbContext<WebzineDbContext>(
         options => options.UseSqlite(builder.Configuration.GetConnectionString("WebzineDbContext"))
     );
 
-//Supprime et crée la base de données
-//Context.Database.ensureDeleted();
-//Context.Database.ensureCreated();
-//SeedDataLocal.Initialize(services);
+// Supprime et crée la base de données
+WebzineDbContext webzineDbContext = new WebzineDbContext();
+webzineDbContext.Database.EnsureDeleted();
+webzineDbContext.Database.EnsureCreated();
+// SeedDataLocal.Initialize(services);
+var db = webzineDbContext.Database.EnsureCreated();
 
 var app = builder.Build();
 
