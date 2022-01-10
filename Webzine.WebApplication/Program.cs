1e6fc502f50755ec55b8a5f409d978edabbe2f2f
TwentyCCent <para.tu.llegar@gmail.com>
2022-01-10 16:10:34 +0100

    fix bug changement de nom

--- Webzine.WebApplication/Program.cs
+++ Webzine.WebApplication/Program.cs
@@ -1,101 +1,99 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using NLog;
 using NLog.Web;
 using Webzine.EntitiesContext;
 using Webzine.Repository;
 using Webzine.Repository.Contracts;
 
-namespace Webzine.WebApplication
-{
-  public class Program
-  {
-    // public static IWebHost BuildWebHost(string[] args) =>
-    //         WebHost.CreateDefaultBuilder(args)
-    //             .UseStartup<Startup>()
-    //             .Build();
-                
-    public static void Main(string[] args)
-    {
-      // BuildWebHost(args).Run();
-      
-      var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
-      logger.Debug("init main");
-      // NLog.LogManager.LoadConfiguration("nlog.config"); Ligne NLog Kevin
+// public static IWebHost BuildWebHost(string[] args) =>
+//         WebHost.CreateDefaultBuilder(args)
+//             .UseStartup<Startup>()
+//             .Build();
+            
 
+  // BuildWebHost(args).Run();
+  
+  var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+  logger.Debug("init main");
+  // NLog.LogManager.LoadConfiguration("nlog.config"); Ligne NLog Kevin
+
+  var builder = WebApplication.CreateBuilder();
+  
+  try
+  {
+
+    // NLog: Setup NLog for Dependency injection
+    // problème au lancement de l'app, l'app bloque en mode debug, et bloque sur le chargement de la config dans launchsettings.json en mode no debug
+    // builder.Logging.ClearProviders(); 
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+
+    // Essai configuration données local ou BDD depuis appsettings.json
+    // var Seed = builder.Configuration.GetSection("ConnectionData");
+    // Console.WriteLine(Seed);
+
+    //builder.Host.UseNLog();
+
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+    builder.Services.AddScoped<ITitreRepository, DbTitreRepository>();
+    builder.Services.AddScoped<IArtisteRepository, DbArtisteRepository>();
+    builder.Services.AddScoped<ICommentaireRepository, DbCommentaireRepository>();
+    builder.Services.AddScoped<IStyleRepository, DbStyleRepository>();
+
+    builder.Services.AddDbContext<WebzineDbContext>(
+          options => options.UseSqlite(builder.Configuration.GetConnectionString("WebzineDbContext"))
+      );
+
+    var app = builder.Build();
+
+    using(var scope = app.Services.CreateScope())
+    {
+      var services = scope.ServiceProvider;
       try
       {
-        var builder = WebApplication.CreateBuilder(args);
+        var webzineDbContext = services.GetRequiredService<WebzineDbContext>();
 
-        // NLog: Setup NLog for Dependency injection
-        // problème au lancement de l'app, l'app bloque en mode debug, et bloque sur le chargement de la config dans launchsettings.json en mode no debug
-        // builder.Logging.ClearProviders(); 
-        builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+        // Supprime et cr�e la base de donn�es
+        webzineDbContext.Database.EnsureDeleted();
+        webzineDbContext.Database.EnsureCreated();
 
-        // Essai configuration données local ou BDD depuis appsettings.json
-        // var Seed = builder.Configuration.GetSection("ConnectionData");
-        // Console.WriteLine(Seed);
-
-        //builder.Host.UseNLog();
-
-        builder.Services.AddControllersWithViews();
-        builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-        builder.Services.AddScoped<ITitreRepository, DbTitreRepository>();
-        builder.Services.AddScoped<IArtisteRepository, DbArtisteRepository>();
-        builder.Services.AddScoped<ICommentaireRepository, DbCommentaireRepository>();
-        builder.Services.AddScoped<IStyleRepository, DbStyleRepository>();
-
-        var app = builder.Build();
-
-        using(var scope = app.Services.CreateScope())
-        {
-          var services = scope.ServiceProvider;
-          try
-          {
-            var webzineDbContext = services.GetRequiredService<WebzineDbContext>();
-
-            // Supprime et cr�e la base de donn�es
-            webzineDbContext.Database.EnsureDeleted();
-            webzineDbContext.Database.EnsureCreated();
-
-            // services.GetService<DbTitreRepository>().Database.Migrate();
-            // Initialisation de la base de donn�es
-            SeedDataLocal.InitialisationDB(webzineDbContext);
-          }
-          catch (Exception e)
-          {
-            var log = services.GetRequiredService<ILogger<Program>>();
-            log.LogError(e, "An error occurred seeding the DB.");
-          }
-        }
-
-        app.UseStaticFiles();
-        app.UseRouting();
-
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllerRoute(
-              name: "areas",
-              pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-            endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-        });
-
-        app.Run();
+        // services.GetService<DbTitreRepository>().Database.Migrate();
+        // Initialisation de la base de donn�es
+        SeedDataLocal.InitialisationDB(webzineDbContext);
       }
-      catch (Exception exception)
+      catch (Exception e)
       {
-          // NLog: catch setup errors
-          logger.Error(exception, "Stopped program because of exception");
-          throw;
-      }
-      finally
-      {
-          // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-          NLog.LogManager.Shutdown();
+        var log = services.GetRequiredService<ILogger<Program>>();
+        log.LogError(e, "An error occurred seeding the DB.");
       }
     }
 
+    app.UseStaticFiles();
+    app.UseRouting();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+          name: "areas",
+          pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+    });
+
+    app.Run();
   }
-}
+  catch (Exception exception)
+  {
+      // NLog: catch setup errors
+      logger.Error(exception, "Stopped program because of exception");
+      throw;
+  }
+  finally
+  {
+      // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+      NLog.LogManager.Shutdown();
+  }
